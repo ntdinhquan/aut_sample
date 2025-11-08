bf9e4d74ae00769911aee245d6cba4a6514b4b10
ntdinhquan <ntdquan2012@gmail.com>
2025-11-08 20:22:39 +0700

    update locale

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
-import createNextIntlPlugin from "next-intl/plugin";
-// const withNextIntl = createNextIntlPlugin();
 
+const nextConfig: NextConfig = {
+  /* config options here */
+};
 
-const nextConfig: NextConfig = {};
-
-const withNextIntl = createNextIntlPlugin();
-export default withNextIntl(nextConfig);
+export default nextConfig;
